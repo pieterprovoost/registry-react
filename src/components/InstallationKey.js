3f0359f530fc6557e7c0391e769cc5a7f6d83d95
Thomas Stjernegaard <tsjeppesen@gbif.org>
2018-08-22 11:03:12 +0200

    initial work on installations

---
+++ src/components/InstallationKey.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import axios from "axios";
+
+class InstallationKey extends React.Component {
+    constructor(props) {
+        super(props);
+
+ 
+        
+    }
+    
+    componentWillMount () {
+      //  const { match: { params: {installationKey} } } = this.props;
+      //  console.log(JSON.stringify(this.props.params))
+      const { match: { params: {installationKey} } } = this.props;
+      this.setState({installationKey: installationKey})
+     /* axios(`https://api.gbif.org/v1/installation/${installationKey}`).then((result) => {
+        that.setState({installationKey: installationKey, data: result.data})
+      }) */
+      
+    }
+
+
+   
+    render() {
+        const { match: { params: {installationKey} } } = this.props;
+     //  const {data: {title}} = this.state;
+        return (
+        <main>
+            {installationKey}
+        </main>
+        )
+    }
+  }
+
+export default InstallationKey;
